6dd160ffecb6ac61bdbb038232bae3970a30f75b
GL~ <gleblobacev07@gmail.com>
2023-11-01 18:22:33 +0300

    Commit1

---
+++ ext.py
@@ -0,0 +1,15 @@
+with open ('text2.txt') as f:
+    lines = f.readlines()
+    print('text2.txt')
+    print(len(lines))
+    print(lines[0])
+
+
+with open ('text1.txt') as v:
+    lines = v.readlines()
+    print('text1.txt')
+    print(len(lines))
+    print(lines[0])
+    print(lines[1])
+
+
